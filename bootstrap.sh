c3ede29d846984f16f42619470c9a0069537729d
David Julia <dajulia3@gmail.com>
2016-03-13 18:12:20 -0500

    Make bootstrap script executable

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,4 @@ echo "Now run guard and hack away at some sequence diagrams...
 I use the LivePage chrome plugin to watch for file system changes and autoreload
 see https://chrome.google.com/webstore/detail/livepage/pilnojpmdoofaelbinaeodfpjheijkbh
 "
+
