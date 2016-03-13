4d491fc277a6ea3f8f3b5c40b9d4753b2366472e
David Julia <dajulia3@gmail.com>
2016-03-13 15:08:15 -0500

    add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pip install -r requirements.txt
+bundle install
+echo "Now run guard and hack away at some sequence diagrams...
+I use the LivePage chrome plugin to watch for file system changes and autoreload
+see https://chrome.google.com/webstore/detail/livepage/pilnojpmdoofaelbinaeodfpjheijkbh
+"
